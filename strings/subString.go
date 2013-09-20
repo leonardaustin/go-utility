6447334a4e56d14bfcf0451c7ac4242d99f08d01
leonardaustin <leonardaustin@gmail.com>
2013-09-20 23:39:37 +0100

    subString search method created.

---
+++ strings/subString.go
@@ -0,0 +1,29 @@
+package stringsextra
+
+import(
+	"strings"
+	"regexp"
+)
+
+func subStringSearch(content []byte, startKeyword string, endKeyword string, removeWhiteSpace bool) string {
+
+	// Remvoe whitespace
+	if(removeWhiteSpace){
+		re := regexp.MustCompile(`\s+`)
+		content = re.ReplaceAll(content, []byte(""))
+	}
+
+	// Find latitude start
+	stringStart := strings.Index(string(content), startKeyword) + len(startKeyword)
+
+	// Take substring with ~100 characters
+	subString := string(content[stringStart:])
+
+	// Find potition of first (next) commar + get latitide
+	endPosition := strings.Index(subString, endKeyword)
+
+	// Whats between them!
+	result := string(subString[0:endPosition])
+
+	return result
+}
