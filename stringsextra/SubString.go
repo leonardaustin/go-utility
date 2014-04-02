8277a7a76ded310e8e154bdeedb6597b2b730d12
Leonard Austin <leonardaustin@gmail.com>
2014-04-02 10:45:04 +0100

    Update SubString.go

--- stringsextra/SubString.go
+++ stringsextra/SubString.go
@@ -13,13 +13,13 @@ func SubStringSearch(content []byte, startKeyword string, endKeyword string, rem
 		content = re.ReplaceAll(content, []byte(""))
 	}
 
-	// Find latitude start
+	// Find start
 	stringStart := strings.Index(string(content), startKeyword) + len(startKeyword)
 
 	// Take substring with ~100 characters
 	subString := string(content[stringStart:])
 
-	// Find potition of first (next) commar + get latitide
+	// Find potition of first (next) comma + get latitide
 	endPosition := strings.Index(subString, endKeyword)
 
 	// Whats between them!
